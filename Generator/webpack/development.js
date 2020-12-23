1a3e2a3b745f6bda127adb7d5582b0f69f7d8e92
mosssiiiiiii <mossii.js@gmail.com>
2020-12-23 13:44:27 +0330

    Initial commit

---
+++ Generator/webpack/development.js
@@ -0,0 +1,115 @@
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const srcDir = path.join(__dirname, './../../src');
+const distDir = path.join(__dirname, './../dist');
+
+const client = {
+    name: 'client',
+    mode:'development',
+    target: 'web',
+    entry: ['webpack-hot-middleware/client?name=client&reload=true', `${srcDir}/client.js`],
+    output: {
+        filename: 'client.js',
+        publicPath: distDir,
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules[\\\/])/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                            plugins: [
+                                "@babel/plugin-transform-runtime",
+                                "@babel/plugin-proposal-object-rest-spread",
+                                ["@babel/plugin-proposal-decorators", { "legacy": true }],
+                                ["@babel/plugin-proposal-class-properties", { "loose" : true }]
+                              ]
+                        }
+                    },
+                    {
+                        loader: 'eslint-loader'
+                    }
+                ],
+            },
+            {
+              test: /\.(css|scss)$/,
+              use: [
+                  {
+                      loader: 'css-hot-loader?cssModule=true',
+                  },
+                  {
+                      loader: MiniCssExtractPlugin.loader
+                  },
+                  {
+                      loader: 'css-loader',
+                      options: {
+                          sourceMap: true,
+                          importLoaders: 1
+                      }
+                  },
+              ]
+          },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }),
+        new Dotenv({systemvars: true}),
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+};
+
+const server =     {
+    name: 'server',
+    mode:'development',
+    target: 'node',
+    entry: ['webpack-hot-middleware/client?name=server&reload=true', `${srcDir}/server.js`],
+    output: {
+        filename: 'server.js',
+        libraryTarget: 'commonjs2',
+        publicPath: path.resolve(process.cwd(), '.' + '/dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules[\\\/])/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                            plugins: [
+                                "@babel/plugin-transform-runtime",
+                                "@babel/plugin-proposal-object-rest-spread",
+                                ["@babel/plugin-proposal-decorators", { "legacy": true }],
+                                ["@babel/plugin-proposal-class-properties", { "loose" : true }]
+                              ]
+                        }
+                    },
+                    {
+                        loader: 'eslint-loader'
+                    }
+                ],
+            },
+
+        ],
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
+
+
+module.exports = [client,server];
---
+++ Generator/webpack/production.js
@@ -0,0 +1,144 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StatsPlugin = require('stats-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const distDir = path.resolve(process.cwd(), './dist');
+const srcDir = path.resolve(process.cwd(), './src');
+
+const client = {
+        name: 'client',
+        mode: 'production',
+        target: 'web',
+        performance: {hints: false},
+        entry: [`${srcDir}/client.js`],
+        output: {
+            path: distDir,
+            filename: 'client.js',
+            publicPath: distDir
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules[\\\/])/,
+                    use: [
+                        {
+                            loader: 'babel-loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.(css|scss)$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                // modules: true,
+                                // localIdentName: '[local]__[hash:base64:5]',
+                                // sourceMap: true,
+                                importLoaders: 1
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                // sourceMap: true,
+                                outputStyle: 'compressed',
+                                includePaths: [`${srcDir}/App/App/_style/module`]
+                            }
+                        }
+                    ]
+                }
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            }),
+            new Dotenv({systemvars: true}),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+        ],
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        output: {
+                            comments: false,
+                        }
+                    },
+                    extractComments: {
+                        banner: false
+                    },
+                    extractComments: false,
+                }),
+            ]
+        }
+    };
+const server = {
+    name: 'server',
+    mode: 'production',
+    target: 'node',
+    performance: {hints: false},
+    entry: [`${srcDir}/server.js`],
+    output: {
+        path: distDir,
+        filename: 'server.js',
+        libraryTarget: 'commonjs2',
+        publicPath: distDir,
+    },
+    externals: {
+        bufferutil: 'commonjs bufferutil',
+        'utf-8-validate': 'commonjs utf-8-validate',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules[\\\/])/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: 'isomorphic-style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            // sourceMap: true,
+                            outputStyle: 'compressed',
+                            includePaths: [`${srcDir}/App/App/_style/module`]
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+    plugins: [
+
+        new StatsPlugin('stats.json', {
+            chunkModules: true,
+            modules: true,
+            chunks: true,
+            exclude: [/node_modules[\\\/]react/],
+        }),
+    ],
+};
+module.exports = [client,server];
